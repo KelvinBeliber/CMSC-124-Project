5e397027e9705a8b4fc9cb6e83885e1db71a3414
gavirttt <158991290+gavirttt@users.noreply.github.com>
2024-11-21 04:19:22 +0800

    added more rules

--- syntax.py
+++ syntax.py
@@ -1,19 +1,30 @@
 import lexical
 import os
 
-def vardec(text, start, i, syntaxResult):
+def vardec(text, start, i, declared_vars, syntaxResult):
     for line in range(start, len(text.splitlines())):
-        lexeme = lexical.lex(text.splitlines()[lexeme].lstrip().rstrip())
+        lexeme = lexical.lex(text.splitlines()[line].lstrip().rstrip())
         if lexeme is not None:
-            ## skips comments
             index = next((i for i, sublist in enumerate(lexeme) if 'Comment line' in sublist), -1)
             if index != -1:
                 continue
-            if ['OBTW','Comment Delimiter'] in lexeme:
-                return 
+            if ['OBTW', 'Comment Delimiter'] in lexeme:
+                return declared_vars, syntaxResult
             if ['TLDR', 'Comment Delimiter'] in lexeme:
-                return
-    return [line, i, syntaxResult]
+                return declared_vars, syntaxResult
+
+            for j in range(len(lexeme)):
+                if lexeme[j][0] == 'I HAS A':
+                    var_name = lexeme[j + 1][0]
+                    if var_name in declared_vars:
+                        syntaxResult += f"syntax error at line {line + 1}: Variable '{var_name}' already declared\n"
+                    else:
+                        declared_vars.append(var_name)
+
+                    if j + 2 < len(lexeme) and lexeme[j + 2][0] != 'ITZ':
+                        syntaxResult += f"syntax error at line {line + 1}: Missing ITZ for variable '{lexeme[j + 1][0]}' initialization\n"
+
+    return declared_vars, syntaxResult
 
 
 def syntax(text):
@@ -23,7 +34,9 @@ def syntax(text):
     buhbye = -1
     obtw = -1
     tldr = -1
+    declared_vars = []
     syntaxResult = ''
+    literals = ['Type Literal', 'TROOF Literal', 'NUMBAR Literal', 'NUMBR Literal', 'YARN Literal']
     for line in range(0, len(text.splitlines())):
         lexeme = lexical.lex(text.splitlines()[line].lstrip().rstrip())
         if lexeme is not None:
@@ -31,7 +44,7 @@ def syntax(text):
             index = next((i for i, sublist in enumerate(lexeme) if 'Comment line' in sublist), -1)
             if index != -1:
                 continue
-
+            # print(f'{lexeme}\n') <-- debugging
             # Remove 'OBTW' & 'TLDR' multi-line comment lexemes
             if obtw==1:
                 if ['TLDR','Comment Delimiter'] not in lexeme:
@@ -55,26 +68,48 @@ def syntax(text):
                 if lexeme[i][0] == 'HAI' and hai == -1 and kthxbye == -1:
                     hai = 1
                 # check for declaration keyword
+                if lexeme[i][0] == 'WAZZAP' or lexeme[i][0] == 'BUHBYE' and wazzup == 0 and buhbye == 0:
+                    syntaxResult += f"syntax error at line {line+1}: WAZZUP-BUHBYE block has already been declared\n"
+                    break
                 if lexeme[i][0] == 'WAZZUP':
                     if wazzup != -1:
                         syntaxResult += f"syntax error at line {line+1}: WAZZUP has already been declared\n"
+                        break
                     if buhbye != -1:
                         syntaxResult += f"syntax error at line {line+1}: WAZZUP cannot be declared after BUHBYE\n"
+                        break
                     if wazzup == -1 and buhbye == -1:
-                        vardecResult = vardec(text, line, i, syntaxResult)
-                        i = vardecResult[0]
+                        vardecResult = vardec(text, line, i, declared_vars, syntaxResult)
+                        declared_vars = vardecResult[0]
                         syntaxResult = vardecResult[1]
                         wazzup = 1
+                        continue
                 if lexeme[i][0] == 'BUHBYE':
-                    if wazzup != 0:
+                    if wazzup != 1:
                         syntaxResult += f"syntax error at line {line+1}: BUHBYE declared without a WAZZUP\n"
+                        break;
                     elif buhbye != -1:
                         syntaxResult += f"syntax error at line {line+1}: BUHBYE has already been declared\n"
+                        break
                     else:
                         wazzup = 0
                         buhbye = 0
-                    # check for variable declarations
-                    # check for wazzup keyword
+                        continue
+                # printing output
+                # if lexeme[i][0] == 'VISIBLE':
+                #     while(i<len(lexeme)):
+                #         i+=1
+                #         if lexeme[i][1] not in literals or lexeme[i][1] != 'Identifier':
+                #             syntaxResult += f"syntax error at line {line+1}: Incorrect VISIBLE syntax!\n"
+                #             break
+                #     if i>=len(lexeme):
+                #         break
+                # taking input
+                if lexeme[i][0] == 'GIMMEH':
+                    if lexeme[i+1][1] != 'Identifier':
+                        syntaxResult += f"syntax error at line {line+1}: Incorrect GIMMEH syntax!\n"
+                        break
+                    continue
                 # check for 
     if len(syntaxResult)==0:
         return "syntax correct"
